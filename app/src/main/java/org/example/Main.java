e7b4d3473c2029abe74d881002808b1da14e7202
Joel Figueirido <joelfigueiridomolares@gmail.com>
2024-03-21 22:16:19 +0100

    cambios de mierda

--- app/src/main/java/org/example/Main.java
+++ app/src/main/java/org/example/Main.java
@@ -9,22 +9,16 @@ public class Main {
 
         System.out.println("Bienvenido al juego de póker!");
 
-        // Solicitar al usuario que ingrese el número de jugadores
+       
         System.out.print("Ingrese el número de jugadores: ");
         int numPlayers = scanner.nextInt();
-
-        // Crear una instancia de la clase Deck
         Deck deck = new Deck();
-
-        // Repartir cartas a los jugadores
         List<List<Card>> hands = deck.dealCards(numPlayers);
 
-        // Mostrar las manos de los jugadores
+       
         for (int i = 0; i < numPlayers; i++) {
             System.out.println("Mano del Jugador " + (i + 1) + ": " + hands.get(i));
         }
-
-        // Cerrar el scanner
         scanner.close();
     }
 }*/
