2a4a2100603d38b1405aa1867169ef5b4d40b385
Joel Figueirido <joelfigueiridomolares@gmail.com>
2024-03-21 11:02:33 +0100

    "Copiamos los datos que llevabamos avanzados"

--- app/src/main/java/org/example/Card.java
+++ app/src/main/java/org/example/Card.java
@@ -1,15 +1,15 @@
 package org.example;
 
 public class Card {
-    private String suit; 
-    private String value; 
+    private String suit; // Palos: corazones, diamantes, tréboles, picas
+    private String value; // Valor de la carta: 2-10, J (Jack), Q (Queen), K (King), A (Ace)
 
     public Card(String suit, String value) {
         this.suit = suit;
         this.value = value;
     }
 
- 
+    // Métodos para obtener el palo y el valor de la carta
     public String getSuit() {
         return suit;
     }
@@ -18,7 +18,7 @@ public class Card {
         return value;
     }
 
-   
+    // Método para representar la carta como una cadena de texto
     @Override
     public String toString() {
         return value + " of " + suit;
--- app/src/main/java/org/example/Table.java
+++ app/src/main/java/org/example/Table.java
@@ -9,30 +9,31 @@ public class Table {
         this.communityCards = new ArrayList<>();
     }
 
-    
+    // Método para repartir las cartas del flop (primeras 3 cartas comunitarias)
     public void dealFlop(Deck deck) {
         dealCards(deck, 3);
     }
 
-    
+    // Método para repartir la carta del turn (cuarta carta comunitaria)
     public void dealTurn(Deck deck) {
         dealCards(deck, 1);
     }
 
-    
+    // Método para repartir la carta del river (quinta carta comunitaria)
     public void dealRiver(Deck deck) {
         dealCards(deck, 1);
     }
 
-    
+    // Método genérico para repartir cartas en la mesa
     private void dealCards(Deck deck, int numCards) {
         List<Card> cards = deck.getCards();
         for (int i = 0; i < numCards; i++) {
-            communityCards.add(cards.remove(0));
+            communityCards.add(cards.remove(0)); // Remover la carta del mazo y agregarla a la mesa
         }
     }
-      
+      // Método para obtener las cartas comunitarias en la mesa
       public List<Card> getCommunityCards() {
         return communityCards;
     }
+    // Otros métodos de la clase Table...
 }
