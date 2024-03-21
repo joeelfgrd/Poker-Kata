cd569a523f92b4aa7a47829e359738745c70005f
Joel Figueirido <joelfigueiridomolares@gmail.com>
2024-03-21 12:14:16 +0100

    HandRank

--- app/src/main/java/org/example/HandRank.java
+++ app/src/main/java/org/example/HandRank.java
@@ -1,6 +1,14 @@
 package org.example;
 
 public enum HandRank {
-    HIGH_CARD, // Enumeración para clasificar las manos de poker
-    // Otras clasificaciones de manos podrían agregarse aquí
+    HIGH_CARD,   // Carta alta
+    ONE_PAIR,    // Un par
+    TWO_PAIR,    // Dos pares
+    THREE_OF_A_KIND, // Trío
+    STRAIGHT,    // Escalera
+    FLUSH,       // Color
+    FULL_HOUSE,  // Full
+    FOUR_OF_A_KIND, // Poker
+    STRAIGHT_FLUSH, // Escalera de color
+    ROYAL_FLUSH  // Escalera real
 }
